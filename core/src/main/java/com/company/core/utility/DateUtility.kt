40e5c0ba2132f3f7c353748e963cb7559f0212f8
W Rangga <willybrodus.khaisar@iconpln.co.id>
2021-06-16 06:56:37 +0700

    perbaikan performance

--- core/src/main/java/com/company/core/utility/DateUtility.kt
+++ core/src/main/java/com/company/core/utility/DateUtility.kt
@@ -5,8 +5,9 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun String.formatDate(): String {
-    val input = SimpleDateFormat("yyyy-MM-dd")
-    val output = SimpleDateFormat("dd MMMM yyyy")
+    val locale = Locale("id","ID")
+    val input = SimpleDateFormat("yyyy-MM-dd",locale)
+    val output = SimpleDateFormat("dd MMMM yyyy", locale)
     return try {
         val inputDate : Date? = input.parse(this) // parse input
         inputDate?.let {
